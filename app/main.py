811112b993c7e3b69341f956cf1fe696e27685d8
Marco063 <95501065+Marc064@users.noreply.github.com>
2024-08-01 00:00:19 -0500

    Add cors configuration

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from app.routes import translate
-
+from starlette.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las orígenes. Para producción, especifica los dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
+
 
 app.include_router(translate.router)
